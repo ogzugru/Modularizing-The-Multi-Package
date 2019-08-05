1f1a3eda34e9af92a72ac662903792555aeadce5
ogzugru <ogzugru@gmail.com>
2019-08-05 10:32:30 +0300

    project started

---
+++ vehicle-models/src/main/java/ModularCar.java
@@ -0,0 +1,49 @@
+
+public class ModularCar {
+/*  "make":"Isuzu",
+    "year":1993,
+    "colour":"Goldenrod",
+    "model":"Amigo"*/
+
+    private final String make;
+    private final int year;
+    private final String colour;
+    private final String model;
+
+    public ModularCar(String make, int year, String colour, String model) {
+        this.make = make;
+        this.year = year;
+        this.colour = colour;
+        this.model = model;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    @Override
+    public String toString() {
+        return "" + colour +
+                " " + make +
+                " " + year + " " + model
+                ;
+    }
+
+    public String toCSVString() {
+        return make + "," + year + "," + colour + "," + model;
+    }
+
+
+}
